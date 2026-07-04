1294f4c0427d37361221d1e4b298cd74531366e2
Your Name <your@email.com>
2026-07-04 14:34:57 +0530

    Add Operators.py file with operator types and descriptions

---
+++ Basics/Operators.py
@@ -0,0 +1,10 @@
+# Operators = Operators are symbols used to perform operations on values and variables.
+        # Types of Operators in Python
+
+        # 1. Arithmetic Operators =      +, -, *, /, //, %, **
+        # 2. Assignment Operators =     =, +=, -=, *=, /=, //=, %=, **=
+        # 3. Comparison Operators =     ==, !=, >, <, >=, <=
+        # 4. Logical Operators    =     and, or, not
+        # 5. Identity Operators   =     is, is not
+        # 6. Membership Operators =     in, not in
+        # 7. Bitwise Operators    =     &, |, ^, ~, <<, >>
