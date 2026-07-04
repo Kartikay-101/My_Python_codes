5f60c827a39ae9ba4899cd86c0b826bac8675818
Your Name <your@email.com>
2026-07-04 15:31:12 +0530

    Add examples for assignment, logical, identity, and membership operators in Operators.py

--- Basics/Operators.py
+++ Basics/Operators.py
@@ -8,3 +8,33 @@
         # 5. Identity Operators   =     is, is not
         # 6. Membership Operators =     in, not in
         # 7. Bitwise Operators    =     &, |, ^, ~, <<, >>
+
+# Assignment operators are used to assign values to variables.
+x = 10
+x += 5
+print(x)
+
+# Logical Operators
+    # Q) Check if a person is eligible to vote (18–100).
+age = int(input("\n\tEnter you age : "))
+
+if age >= 18 and age <= 100:
+    print("\nPerson with this age " + str(age) + " is eligible\n")
+else:
+    print("\nPerson with this age " + str(age) + " is not eligible\n")
+
+# Identity Operators = It check whether two variables refer to the same object in memory, not just whether their values are equal.
+x = 10
+y = 10
+
+print(x is y)
+
+# Membership operators = Its check whether a value exists inside a sequence.
+
+fruits = ["Apple", "Mango", "Banana"]
+
+print("Apple" in fruits)
+
+fruits = ["Apple", "Mango", "Banana"]
+
+print("Orange" not in fruits)
