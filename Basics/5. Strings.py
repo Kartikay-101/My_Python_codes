e90999999e926d9a6d8d46b216aac79f887c88aa
Your Name <your@email.com>
2026-07-04 16:16:51 +0530

    Add Strings.py file with string definitions, examples, and length calculations

---
+++ Basics/5. Strings.py
@@ -0,0 +1,27 @@
+# What is a String? = A string is a sequence of characters enclosed in quotes.
+# A character can be: Letters, Numbers, Symbols, Spaces.
+
+# Strings are Immutable || Immutable means: Once a string is created, its characters cannot be changed.
+
+name = 'Kartikay'
+
+address = """\nHouse No. 10
+Greater Noida
+Uttar Pradesh\n"""
+
+print("\n")
+print(name[0])
+print(name[1])
+print(name[2])
+print(name[3])
+print(name[4])
+print(name[5])
+print(name[6])
+print(name[7])
+
+print(address)
+print(type(address))
+print(f"Length of '{name}' = {len(name)}")  # String Length len()
+print(f"Length of 'address' = {len(address)}")  # String Length len()
+
+print('\n')
