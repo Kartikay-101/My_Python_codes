6d2e3c6b249407a6676af0109982a67005b2ce4d
Your Name <your@email.com>
2026-07-05 12:47:53 +0530

    Update Lists.py to clarify list mutability and enhance indexing examples

--- Basics/6. Lists.py
+++ Basics/6. Lists.py
@@ -1,4 +1,4 @@
-# What is a List? = A list is a collection of multiple values stored in a single variable. 
+# What is a List? = A list is a collection of multiple values stored in a single variable. List is Mutable
 
 students = ['kartikay', 'rahul', 'prateek', 'chetan', 'dron', 'sambhav']
 marks = [54, 34, 64, 34, 64, 43, 34]
@@ -15,3 +15,23 @@ print()
 print('Students Name :- ',students,'\n')
 print('Marks :- ', marks,'\n')
 print('Students Subjects :- ', subjects,'\n')
+
+# Indexing in Lists
+print('Indexing in Lists\n')
+print(students[0])
+print(marks[-3])
+print(subjects[3])
+print(marks[::-1])
+print(students[::-1])
+print(subjects[::-1])
+
+
+# List are Mutable
+print('\nMutable\n ---------------')
+students[2] = 'shivam'
+print(students)
+subjects[1] = 'No subjects'
+print(subjects)
+marks[4] = 'Null'
+print(marks)
+print()
