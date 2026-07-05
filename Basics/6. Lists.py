918b3bd9a1302d92dbed1c7f7c2e523f004a609d
Your Name <your@email.com>
2026-07-05 12:25:18 +0530

    Add Lists.py file to demonstrate list creation and display student data

---
+++ Basics/6. Lists.py
@@ -0,0 +1,17 @@
+# What is a List? = A list is a collection of multiple values stored in a single variable. 
+
+students = ['kartikay', 'rahul', 'prateek', 'chetan', 'dron', 'sambhav']
+marks = [54, 34, 64, 34, 64, 43, 34]
+subjects = [
+    ['python', 'sql', 'r'],
+    ['excel', 'sql', 'power bi'],
+    ['statistics', 'r', 'python'],
+    ['tableau', 'sql', 'excel'],
+    ['python', 'machine learning', 'sql'],
+    ['power bi', 'tableau', 'excel']
+]
+
+print()
+print('Students Name :- ',students,'\n')
+print('Marks :- ', marks,'\n')
+print('Students Subjects :- ', subjects,'\n')
