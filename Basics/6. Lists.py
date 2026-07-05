04010cc0e5663bd36b388629c511a7ca87e6ae62
Your Name <your@email.com>
2026-07-05 14:05:15 +0530

    Enhance Lists.py with additional list methods and examples, including append, insert, remove, sort, and copy functionalities.

--- Basics/6. Lists.py
+++ Basics/6. Lists.py
@@ -40,3 +40,70 @@ print()
 
 print(students[1:-1])
 print(subjects[0:-1:2])
+marks[1:1]=[12,12] #Insert Values Using Slice Assignment
+print(marks)
+marks[1:8] = [] #Delete Elements Using Slice Assignment
+print(marks)
+
+# List Methods = A method is a function that belongs to an object.
+print('\n List Method :- \n ------------------------')
+
+print(students)
+students.append('karan')  # It adds one element to the end of a list.
+print(students)
+students.append(['dhruv','krish'])
+print(students)
+marks.append(True)
+print(marks)
+
+print()
+numbers = [10, 20, 30]
+numbers.extend([40, 50, 60])    #It adds multiple elements from another iterable (such as a list) to the end of the existing list.
+
+print(numbers)
+
+letters = []
+letters.extend("Python")
+print(letters)
+
+print()
+students.insert(1,'varun') #insert() inserts one element at a specified index.
+print(students)
+
+print('\n',students)
+students.remove('kartikay') # remove() removes the first occurrence of the specified value.
+print(students,'\n')
+
+print('\n',students)
+students.pop(1) # It removes an element by its index. Pop only return delete value in list methods.
+print(students,'\n')
+
+print('\n',students)
+# students.clear() # It clear() removes all elements from a list, but the list itself still exists.
+# students[0:10] = []
+print(students,'\n')
+
+print(students.index('dron')) # It returns the index of the first occurrence of the specified value.
+
+print(marks.count(54))
+
+print()
+fruits = ["Mango", "Apple", "Banana", "Orange"]
+
+fruits.sort()
+
+print(fruits)
+
+fruits.sort(reverse = True)
+print(fruits)
+
+fruits.reverse() # It reverses the current order of the elements.
+print(fruits)
+
+print()
+numbers = [10, 20, 30]
+
+new_numbers = numbers.copy() # It creates a shallow copy of the list.
+
+print(numbers)
+print(new_numbers)
