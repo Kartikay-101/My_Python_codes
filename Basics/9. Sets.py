3ca7f946f20e565dc90368c57426b72d4a591b24
Your Name <your@email.com>
2026-07-11 12:53:03 +0530

    Add set operations examples with explanations and create a new file for dictionaries

--- Basics/9. Sets.py
+++ Basics/9. Sets.py
@@ -84,3 +84,85 @@ b = a.copy()
 
 print(a)
 print(b)
+
+
+# Set Operations
+
+# Interview Notes
+# | → Union
+# & → Intersection
+# - → Difference
+# ^ → Symmetric Difference
+# <= → Subset
+# >= → Superset
+
+print('\n\t Set Operations \n\t----------------')
+
+
+# 1. Union = Meaning: Combine both sets. Duplicates appear only once
+
+A = {1, 2, 3}
+B = {3, 4, 5}
+print(f'\nUnion of A and B is {A.union(B)}  or {A|B}\n')
+
+
+# 2. Intersection = Meaning: Only the common elements.
+
+A = {1, 2, 3, 4}
+B = {3, 4, 5, 6}
+
+print(f"Intersection of A and B {A.intersection(B)} or {A & B}\n")
+
+# 3. Difference = This is where beginners often get confused. as Elements in A but not in B.
+
+A = {1, 2, 3, 4}
+B = {3, 4, 5, 6}
+
+print(f"Difference of A and B {A.difference(B)} or {A - B}\n")
+print(f"Difference of B and A {B.difference(A)} or {B - A}\n")
+
+# 4. Symmetric Difference : Take elements that are not common.
+
+print(f"Symmetric Difference of A and B is {A.symmetric_difference(B)} or {(A ^ B)}\n")
+
+# 5. Subset = A set is a subset if every element of it exists in another set.
+
+A = {1, 2}
+B = {1, 2, 3, 4}
+
+print(A.issubset(B))
+print(A <= B)
+print()
+
+# 6. Superset = Opposite of subset.
+
+A = {1, 2, 3, 4}
+B = {1, 2}
+
+print(A.issuperset(B))
+print(A >= B)
+print()
+
+# 7. Disjoint = Two sets are disjoint if they have no common elements.
+
+A = {1, 2}
+B = {3, 4}
+
+print(A.isdisjoint(B))
+print()
+
+print('\n\t Another Example\n\t------------')
+
+A = {1, 2, 3, 4}
+B = {3, 4, 5, 6}
+
+print("Union:", A | B)
+print("Intersection:", A & B)
+print("Difference:", A - B)
+print("Difference:", B - A)
+print("Symmetric Difference:", A ^ B)
+
+print({1, 2}.issubset(A))
+print(A.issuperset({1, 2}))
+print({10, 20}.isdisjoint(A))
+
