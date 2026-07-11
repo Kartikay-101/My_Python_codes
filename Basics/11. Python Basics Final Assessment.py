5e93c662ffc8c78f7ee7d5a651b17e90eb1d9392
Your Name <your@email.com>
2026-07-11 22:44:36 +0530

    Add initial project guidelines and requirements for Employee & Project Management System

---
+++ Basics/11. Python Basics Final Assessment.py
@@ -0,0 +1,37 @@
+"""
+==========================================================
+            TechNova Solutions Pvt. Ltd.
+        Employee & Project Management System
+==========================================================
+
+BACKGROUND
+
+You recently joined TechNova Solutions as a Python Developer.
+
+The HR department has been maintaining employee records in Excel.
+Management now wants you to build the first version of their
+Employee & Project Management System.
+
+Your job is NOT to build everything at once.
+
+The manager will keep giving you new requirements.
+
+Complete every task in order.
+
+Rules
+
+✔ Don't skip tasks.
+✔ Think first.
+✔ Use the concepts you've already learned.
+
+DO NOT USE
+------------
+Functions
+Files
+Exception Handling
+OOP
+Modules (except copy if required)
+
+Only use the topics you've already learned.
+
+"""
