f2069da8e4410ad6fc0932d6a28eda3db45d5ca4
Your Name <your@email.com>
2026-07-12 16:18:23 +0530

    Add employee registration functionality with input validation

--- Basics/11. Python Basics Final Assessment.py
+++ Basics/11. Python Basics Final Assessment.py
@@ -35,3 +35,34 @@ Modules (except copy if required)
 Only use the topics you've already learned.
 
 """
+
+
+# Phase 1 - Employee Registration (Tasks 1-4)
+
+# Task 1
+# Register 10 employees using user input.
+# Clean the input, convert datatypes, validate email and phone number.
+
+employee = []
+
+for i in range(10):
+    print(f"Employee {i}")
+    emp_id = input('Enter Employee ID :- ').strip()
+    name = input('Enter Employee Name :- ').strip().title()
+    age = input('Enter age :- +91 ').strip()
+    mail = input('Enter Mail ID :- ').strip().casefold()
+    phone_number = input('Enter Phone Number :- ').strip()
+
+emp_id = int(emp_id)
+age = int(age)
+phone_number = int(phone_number)
+
+if '@' in mail and mail.endswith('.com'):
+    print('Valid Mail')
+else:
+    print("Invalid mail")
+
+if phone_number.isdigit() and len(phone_number) == 10 and phone_number[0] in '6789':
+    print("Valid Phone Number")
+else:
+    print("Invalid Phone Number")
