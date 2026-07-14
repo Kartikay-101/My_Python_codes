fd028e5874fdac8234f9c96fb910ec5ae2df24d9
Your Name <your@email.com>
2026-07-14 14:27:37 +0530

    Add ATM example with user authentication and balance withdrawal logic

--- Intermediate/01. Conditional Statements.py
+++ Intermediate/01. Conditional Statements.py
@@ -25,3 +25,37 @@ if  age >=18:
     print(f'\nPerson having age "{age}" is eligible to vote\n')
 else:
     print(f'\nPerson having age "{age}" is not eligible to vote\n')
+
+# 2. Nested if Statements = A nested if means writing an if statement inside another if statement.
+
+# Example (ATM)
+
+user_name = input('\n\t ⭐ Enter User Name here :- ').strip().title()
+print()
+print("="*50)
+print('\t Most Welcome to MKB ATM')
+print("="*50)
+
+print(f"\n\t  Hello {user_name}!!, Nice to see you again.\n")
+
+pin_code = 1234
+
+pin = int(input('Enter Pin = '))
+
+balance = 100000
+
+if pin == pin_code:
+    print("\n ✅ Verified Customer")
+    print("\n\t Do you want to withdraw money from your account? \n\t-----------------")
+    withdraw = float(input("Enter Withdrawl Amount in 'Rs' - "))
+    if withdraw <= balance:
+        print(f'\n Your account has been debited by {withdraw} amount')
+        print(f"\nNow Your bank balance is {balance - withdraw}")
+        if (balance - withdraw) % 2 == 0:
+            print(f"\nYour bank balance is even number {balance - withdraw}")
+        else:
+            print(f"\nYour bank balance is odd number {balance - withdraw}")
+    else:
+        print('You Have not sufficient balance to withdraw from you account')
+else:
+    print("\n ❌ Unknown Customer")
