4230cf6e86d154a2d6a672cc48fce48b52d41ffd
Your Name <your@email.com>
2026-07-19 14:29:15 +0530

    ATM refined

--- 04 Projects/01 ATM.py
+++ 04 Projects/01 ATM.py
@@ -40,11 +40,8 @@ if valid:
     account_holder['balance'] = float(account_holder['balance'])
 
     print(f'\n\t{'-'*20} Congratulation {'-'*20}\n\t ✅ Customer Acc. is created Successfully.')
-    print(f'\nAccount Holder Information: \n {'-'*20}')
-    for keys,values in account_holder.items():
-        print(f'*{keys.upper()} = {values}')
-    
-    continue_account = input('Do you want to continue with your account? (Yes/No) ').strip().upper()
+        
+    continue_account = input('\nDo you want to continue with your account? (Yes/No) ').strip().upper()
 
     if continue_account == 'YES' or continue_account == 'Y':
 
@@ -54,36 +51,61 @@ if valid:
             print("PIN verified.")
             print(f"\n{'~'*20} 🏧 ATM Menu {'~'*20}")
             print(f"{' '*20}~~~~~~~~~~~~~~{' '*20}")
+            while True:
 
-            print(f"\n\t----> 1. Deposit💵 \n\t----> 2. Withdraw📉 \n\t----> 3. Balance Enquiry⚖️ \n\t----> 4. Exit💨 \n")
+                print(f"\n\t----> 1. Deposit💵 \n\t----> 2. Withdraw📉 \n\t----> 3. Balance Enquiry⚖️ \n\t----> 4. Account Info.🏦 \n\t----> 5. Change Pin🫆 \n\t----> 6. Exit💨 \n")
 
-            choice = input('Enter your choice - ').strip().lower()
+                choice = input('Enter your choice - ').strip().lower()
 
-            if choice == 'deposit' or choice == '1':
+                if choice in ('deposit', '1'):
 
-                d_amount = float(input('~ Amount :- ').strip())
-                account_holder['balance'] = float(account_holder['balance']) + d_amount
-                print(f'\n\t✅Your Account has been credited by an amount {d_amount} 💵\n')
-                print(f'\n\t Total Acc. Balance after credited is {account_holder['balance']}')
+                    d_amount = float(input('~ Amount to be Deposit :- ').strip())
+                    if d_amount <=0:
+                        print('\n❌ Enter an amount greater than zero.')
+                    else:
+                        account_holder['balance'] += d_amount
+                        print(f'\n\t✅ Your Account has been credited by an amount {d_amount} 💵\n')
+                        print(f'\n\t Total Acc. Balance after credited is {account_holder['balance']}')
 
-            elif choice == 'withdraw' or choice == '2':
+                elif choice in ('withdraw', '2'):
 
-                w_amount = float(input('~ Amount :- ').strip())
-                if w_amount <= 0:
-                    print("❌ Enter an amount greater than zero.")
-                elif w_amount > account_holder['balance']:
-                    print("❌ Insuffiecient Balance")
+                    w_amount = float(input('~ Amount :- ').strip())
+                    if w_amount <= 0:
+                        print("❌ Enter an amount greater than zero.")
+                    elif w_amount > account_holder['balance']:
+                        print("❌ Insuffiecient Balance")
+                    else:
+                        account_holder['balance'] -= w_amount
+
+                    print(f'\n\t✅Your Account has been debited by an amount {w_amount} 💵\n')
+                    print(f'\n\t Total Acc. Balance after withdrawal is {account_holder['balance']}')
+
+                elif choice in ('balance', '3'):
+                    print(f'\n\t✅ Account Balance = Rs. {account_holder['balance']} 💵\n')
+
+                elif choice in ('account info', '4'):
+                    print(f'\nAccount Holder Information: \n {'-'*20}')
+                    for keys,values in account_holder.items():
+                        print(f'*{keys.upper()} = {values}')
+
+                elif choice in ('change pin', '5'):
+                    if account_holder['pincode'] == input(f'\nEnter Existing Pin of {account_holder["name"]} Acc. = '):
+                        print(f"\n\tCreate New Pincode for your bank 🏦\n\t{'-'*20}")
+                        new_pincode = input("New Pin Code🫆 = ")
+                        if new_pincode.isdigit() == True and len(new_pincode) == 4:
+                            print(f"⭐ New Pin has created :- ({new_pincode})")
+                            account_holder['pincode'] = new_pincode
+                        else:
+                            print('❌ Invalid PIN. It must contain exactly 4 digits.')
+                    else:
+                        print("\n\t❌ Invalid Pin")
+
+                elif choice in ('exit', '6'):
+                    print('\n\tThank you for using MKB ATM. Visit again!')
+                    break
+
                 else:
-                    account_holder['balance'] = float(account_holder['balance']) - w_amount
-
-                print(f'\n\t✅Your Account has been debited by an amount {w_amount} 💵\n')
-                print(f'\n\t Total Acc. Balance after withdrawal is {account_holder['balance']}')
-
-            elif choice == 'balance' or choice == '3':
-                print(f'\n\t✅ Account Balance = Rs. {account_holder['balance']} 💵\n')
-
-            else:
-                print(f"\n\t{'~'*20} Thank You for using MKB ATM {'~'*20}\n\t{'~'*20} Visit Again {'~'*20}\n\t{'~'*20} MKB ATM ⭐ {'~'*20}")
+                    print(f"\n\t❌ Invalid choice. Please select 1, 2, 3, 4, 5 or 6.\n\n")
 
         else:
             print('\n\t❌ Invalid Pin')
